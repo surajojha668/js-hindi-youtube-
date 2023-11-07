88c097f9b21b3a746b77b5ee6ed21afdcc959755
Suraj Ojha <90503155+surajojha668@users.noreply.github.com>
2023-11-07 17:36:38 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 144553
+let accountEmail ="surajojha@gmail.comm" 
+var accountPassword ="12345"
+accountCity ="Jaipur"  //bina kuch likhe variable creation , but this is not recommended
+let accountState;
+
+//In var there is block problem so we use in let for creating  new  variables
+
+// accountId =2
+accountEmail ="ojha@gmail.comm"
+ accountPassword ="25455"
+accountCity ="dhanbad"
+
+console.log(accountId);
+console.table([accountCity,accountEmail,accountId,accountState])
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,30 @@
+"use strict"; // treat all JS code as newer version
+
+// alert( 3 + 3) // we are using nodejs, not browser  // in browser it will give 6 but in nodejs it will give error
+
+console.log(3 
+    +
+     3) // code readability should be high
+
+console.log("Hitesh")
+
+
+let name = "hitesh"
+let age = 18
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53 (range of number)
+// bigint (for bigger numbers in trading , facebook etc.)
+// string => ""
+// boolean => true/false
+// null => standalone value (means the variable is empty) // null is object
+// undefined => (when value is not defined)
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
+console.log(typeof "suraj")
